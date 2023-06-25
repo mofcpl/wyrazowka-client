43033da7a9af5c8e60dcf894b47d5354d3a0e652
Dominik Z <zbrogdom@gmail.com>
2023-06-25 16:59:59 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DescComponent } from './desc/desc.component';
+import { CounterComponent } from './counter/counter.component';
+import { LettersComponent } from './letters/letters.component';
+import { ButtonComponent } from './button/button.component';
+import { WordsComponent } from './words/words.component';
+import { PanelComponent } from './panel/panel.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescComponent,
+    CounterComponent,
+    LettersComponent,
+    ButtonComponent,
+    WordsComponent,
+    PanelComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
